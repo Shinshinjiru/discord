2a06aaa18ca78e9a59a7327b2169fe280bd5149b
manulaiko <manulaiko@gmail.com>
2021-04-24 17:26:11 +0200

    Initial commit

---
+++ src/main/java/com/manulaiko/shinshinjiru/discord/DiscordApplication.java
@@ -0,0 +1,14 @@
+package com.manulaiko.shinshinjiru.discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author manulaiko <manulaiko@gmail.com>
+ */
+@SpringBootApplication
+public class DiscordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordApplication.class, args);
+    }
+}
